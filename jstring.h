79ad70865ff469bcfc754b9d0c2ca9dabcd51437
Josiah Bruner <josiah@programmer.net>
2015-06-05 08:29:29 -0400

    Add conversion functions

--- jstring.h
+++ jstring.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <sstream>
 
 namespace JCPP {
   std::string test() {
@@ -36,5 +37,36 @@ namespace JCPP {
     }
     std::cout << "\n===\n";
     return stringsToReturn;
+  }
+
+  // Type Conversions
+  std::string IntToString(int anInt) {
+    std::stringstream ss;
+    ss << anInt;
+    std::string aString = ss.str();
+    return aString;
+  }
+
+  std::string FloatToString(float aFloat) {
+    std::stringstream ss;
+    ss << aFloat;
+    std::string aString = ss.str();
+    return aString;
+  }
+
+  std::string BoolToString(bool aBool) {
+    if (aBool) {
+        return "1";
+    }
+    else {
+        return "0";
+    }
+  }
+
+  std::string SizetToString(size_t aSizeT) {
+    std::stringstream ss;
+
+    ss << aSizeT;
+    return ss.str();
   }
 }
